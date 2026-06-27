dbd0c589df6d2cb2f68e7d245c4142cd2f614b5d
nastaysa1337 <nastasyabaklykova@gmail.com>
2026-06-27 19:48:45 +0700

    Add Tailwind config

---
+++ nastasya-digital-brand/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        milk: '#F4EFE7',
+        graphite: '#171513',
+        champagne: '#D7B98A',
+        roseDust: '#B98D87',
+        clay: '#8B6A5B'
+      }
+    }
+  },
+  plugins: []
+};
